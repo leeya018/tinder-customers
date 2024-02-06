c7d62c75c8861154d3f73c2cf12a59cd40128d92
leeya018 <leeyahav018@gmail.com>
2024-02-06 18:49:43 +0200

    t

--- src/util.ts
+++ src/util.ts
@@ -229,4 +229,5 @@ export const instructions = [
   "auto likes for you so you will not have to waist times. ( by Machine learning it will already choose for you )",
   "auto message for all the matches , so you will not need to think what to send them for the first time ( all you have to do is just to continue typing with the women who already answer for the first message). ",
   "it will like back all the women who already liked you , so you will not have to do it by your self",
+  "it can find a match according to search words that you want  it to look for in the bio",
 ]
