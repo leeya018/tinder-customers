0d8d720cd9cd28ce404448012d20845d92973e14
leeya018 <leeyahav018@gmail.com>
2024-04-25 00:46:30 +0300

    favicon

--- src/pages/api/getMatches.ts
+++ src/pages/api/getMatches.ts
@@ -1,14 +1,14 @@
-import nc from "next-connect"
-import { corsMiddleware } from "./validate"
-import type { NextApiRequest, NextApiResponse } from "next"
-import { getMatchesApi } from "@/node/api"
+import nc from "next-connect";
+import { corsMiddleware } from "./validate";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getMatchesApi } from "@/node/api";
 
-const handler = nc({ attachParams: true })
-handler.use(corsMiddleware)
+const handler = nc({ attachParams: true });
+handler.use(corsMiddleware);
 
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
-  const { token, payload } = req.body
-  const result = await getMatchesApi(token, payload)
-  res.status(200).json(result.data)
-})
-export default handler
+  const { token, payload } = req.body;
+  const result = await getMatchesApi(token, payload);
+  res.status(200).json(result.data);
+});
+export default handler;
